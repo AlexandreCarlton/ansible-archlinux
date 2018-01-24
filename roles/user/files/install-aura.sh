40e970fbc8dbfce1c81aede53acc1fb8757adba6
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2018-01-24 17:29:11 +1100

    Remove install-aura.sh script
    This is non-functional and already implemented in the 'aura' role.

--- roles/user/files/install-aura.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# TODO: Seems like we don't need this.
-
-cd /home/alexandre/aura-bin
-# curl --location --remote-name https://aur.archlinux.org/cgit/aur.git/snapshot/aura-bin.tar.gz
-# tar --extract --gzip aura-bin.tar.gz
-# cd aura-bin
-makepkg -sic --noconfirm
