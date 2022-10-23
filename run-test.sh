df0a2d8ed493c70b7e4fbc6db12215faedc8b9f0
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2022-10-23 22:54:01 +1100

    Fix up run-test.sh for latest molecule

--- run-test.sh
+++ run-test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ ! -d .venv ]; then
   python3 -m venv .venv
-  .venv/bin/pip install molecule molecule-docker
+  .venv/bin/pip install "molecule[docker]==4.0.2"
 fi
 
 . .venv/bin/activate
-PY_COLORS=1 ANSIBLE_FORCE_COLOR=1 molecule test --destroy=never -- -v
+ANSIBLE_ROLES_PATH="$(pwd)/roles" PY_COLORS=1 ANSIBLE_FORCE_COLOR=1 molecule test --destroy=never -- -v
